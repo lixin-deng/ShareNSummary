324b52260d98b3f2a0063de79a3a7dacae076e2c
lixin0610 <83501683+lixin-deng@users.noreply.github.com>
2022-09-30 18:25:45 +0930

    1

--- my_project.o.d
+++ my_project.o.d
@@ -1,42 +1,33 @@
-out/clang-release//my_project.o: my_project.cc \
-  /Users/denglixin/omnetpp-5.6.2/include/omnetpp.h MyMsg_m.h \
-  /usr/local/mysql-8.0.22-macos10.15-x86_64/include/mysql.h \
-  /usr/local/mysql-8.0.22-macos10.15-x86_64/include/field_types.h \
-  /usr/local/mysql-8.0.22-macos10.15-x86_64/include/my_list.h \
-  /usr/local/mysql-8.0.22-macos10.15-x86_64/include/mysql_com.h \
-  /usr/local/mysql-8.0.22-macos10.15-x86_64/include/my_command.h \
-  /usr/local/mysql-8.0.22-macos10.15-x86_64/include/my_compress.h \
-  /usr/local/mysql-8.0.22-macos10.15-x86_64/include/mysql/udf_registration_types.h \
-  /usr/local/mysql-8.0.22-macos10.15-x86_64/include/mysql/client_plugin.h \
-  /usr/local/mysql-8.0.22-macos10.15-x86_64/include/mysql/plugin_auth_common.h \
-  /usr/local/mysql-8.0.22-macos10.15-x86_64/include/mysql_version.h \
-  /usr/local/mysql-8.0.22-macos10.15-x86_64/include/mysql_time.h \
-  /usr/local/mysql-8.0.22-macos10.15-x86_64/include/errmsg.h
+out/emcc-release//my_project.o: my_project.cc \
+  /root/omnetpp/include/omnetpp.h MyMsg_m.h mysql.h field_types.h \
+  my_list.h mysql_com.h my_command.h my_compress.h \
+  mysql/udf_registration_types.h mysql/client_plugin.h \
+  mysql/plugin_auth_common.h mysql_version.h mysql_time.h errmsg.h
 
-/Users/denglixin/omnetpp-5.6.2/include/omnetpp.h:
+/root/omnetpp/include/omnetpp.h:
 
 MyMsg_m.h:
 
-/usr/local/mysql-8.0.22-macos10.15-x86_64/include/mysql.h:
+mysql.h:
 
-/usr/local/mysql-8.0.22-macos10.15-x86_64/include/field_types.h:
+field_types.h:
 
-/usr/local/mysql-8.0.22-macos10.15-x86_64/include/my_list.h:
+my_list.h:
 
-/usr/local/mysql-8.0.22-macos10.15-x86_64/include/mysql_com.h:
+mysql_com.h:
 
-/usr/local/mysql-8.0.22-macos10.15-x86_64/include/my_command.h:
+my_command.h:
 
-/usr/local/mysql-8.0.22-macos10.15-x86_64/include/my_compress.h:
+my_compress.h:
 
-/usr/local/mysql-8.0.22-macos10.15-x86_64/include/mysql/udf_registration_types.h:
+mysql/udf_registration_types.h:
 
-/usr/local/mysql-8.0.22-macos10.15-x86_64/include/mysql/client_plugin.h:
+mysql/client_plugin.h:
 
-/usr/local/mysql-8.0.22-macos10.15-x86_64/include/mysql/plugin_auth_common.h:
+mysql/plugin_auth_common.h:
 
-/usr/local/mysql-8.0.22-macos10.15-x86_64/include/mysql_version.h:
+mysql_version.h:
 
-/usr/local/mysql-8.0.22-macos10.15-x86_64/include/mysql_time.h:
+mysql_time.h:
 
-/usr/local/mysql-8.0.22-macos10.15-x86_64/include/errmsg.h:
+errmsg.h:
