74dc51e55d6ff726fc7f9344d3556d4e5a9abece
lixin0610 <83501683+lixin-deng@users.noreply.github.com>
2022-10-05 01:52:23 +1030

    project

--- MyMsg_m.o.d
+++ MyMsg_m.o.d
@@ -1,4 +1,4 @@
-out/emcc-release//MyMsg_m.o: MyMsg_m.cc MyMsg_m.h \
+out/clang-release//MyMsg_m.o: MyMsg_m.cc MyMsg_m.h \
   /root/omnetpp/include/omnetpp.h
 
 MyMsg_m.h:
--- my_project.o.d
+++ my_project.o.d
@@ -1,33 +1,6 @@
-out/emcc-release//my_project.o: my_project.cc \
-  /root/omnetpp/include/omnetpp.h MyMsg_m.h mysql.h field_types.h \
-  my_list.h mysql_com.h my_command.h my_compress.h \
-  mysql/udf_registration_types.h mysql/client_plugin.h \
-  mysql/plugin_auth_common.h mysql_version.h mysql_time.h errmsg.h
+out/clang-release//my_project.o: my_project.cc \
+  /root/omnetpp/include/omnetpp.h MyMsg_m.h
 
 /root/omnetpp/include/omnetpp.h:
 
 MyMsg_m.h:
-
-mysql.h:
-
-field_types.h:
-
-my_list.h:
-
-mysql_com.h:
-
-my_command.h:
-
-my_compress.h:
-
-mysql/udf_registration_types.h:
-
-mysql/client_plugin.h:
-
-mysql/plugin_auth_common.h:
-
-mysql_version.h:
-
-mysql_time.h:
-
-errmsg.h:
