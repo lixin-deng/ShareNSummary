32a59a05430ce4b1d6f5d2a2a32a8ea1fd15fe88
lixin0610 <83501683+lixin-deng@users.noreply.github.com>
2022-10-05 02:24:07 +1030

    aloha

--- MyMsg_m.o.d
+++
@@ -1,6 +0,0 @@
-out/clang-release//MyMsg_m.o: MyMsg_m.cc MyMsg_m.h \
-  /Users/denglixin/omnetpp-5.6.2/include/omnetpp.h
-
-MyMsg_m.h:
-
-/Users/denglixin/omnetpp-5.6.2/include/omnetpp.h:
--- project.o.d
+++
@@ -1,6 +0,0 @@
-out/clang-release//project.o: project.cc \
-  /Users/denglixin/omnetpp-5.6.2/include/omnetpp.h MyMsg_m.h
-
-/Users/denglixin/omnetpp-5.6.2/include/omnetpp.h:
-
-MyMsg_m.h:
