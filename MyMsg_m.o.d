f625ab84b8548da4547397acafe90e3ab62911ea
lixin0610 <83501683+lixin-deng@users.noreply.github.com>
2022-10-05 02:38:50 +1030

    update

---
+++ MyMsg_m.o.d
@@ -0,0 +1,6 @@
+out/emcc-release//MyMsg_m.o: MyMsg_m.cc MyMsg_m.h \
+  /root/omnetpp/include/omnetpp.h
+
+MyMsg_m.h:
+
+/root/omnetpp/include/omnetpp.h:
--- Server.o.d
+++
@@ -1,6 +0,0 @@
-out/emcc-release//Server.o: Server.cc Server.h \
-  /root/omnetpp/include/omnetpp.h
-
-Server.h:
-
-/root/omnetpp/include/omnetpp.h:
