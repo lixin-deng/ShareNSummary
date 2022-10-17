9aaf843f63a8dcb3b9ed121a9e01384c6f997b03
lixin0610 <83501683+lixin-deng@users.noreply.github.com>
2022-10-18 03:02:28 +1030

    add curl

--- project.o.d
+++ project.o.d
@@ -1,6 +1,28 @@
 out/emcc-release//project.o: project.cc /root/omnetpp/include/omnetpp.h \
-  MyMsg_m.h
+  MyMsg_m.h curl/curl.h curl/curlver.h curl/system.h curl/curlbuild.h \
+  curl/curlrules.h curl/easy.h curl/multi.h curl/Availability.h \
+  curl/AvailabilityVersions.h curl/AvailabilityInternal.h
 
 /root/omnetpp/include/omnetpp.h:
 
 MyMsg_m.h:
+
+curl/curl.h:
+
+curl/curlver.h:
+
+curl/system.h:
+
+curl/curlbuild.h:
+
+curl/curlrules.h:
+
+curl/easy.h:
+
+curl/multi.h:
+
+curl/Availability.h:
+
+curl/AvailabilityVersions.h:
+
+curl/AvailabilityInternal.h:
