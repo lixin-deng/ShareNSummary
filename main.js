3b08c9a2d13c5dc71fcd25ac2a656e34bbadd916
lixin0610 <83501683+lixin-deng@users.noreply.github.com>
2022-10-21 12:56:16 +1030

    delete curl

--- main.js
+++
@@ -1,8 +0,0 @@
-import App from './App.svelte';
-
-const root = document.createElement("DIV");
-document.body.appendChild(root);
-setTimeout(() => {
-    const app = new App({ target: root })
-}, 1000)
-
