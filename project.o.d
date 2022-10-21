1157a1f88d7df7d5d3164959a2053feed327bb17
lixin0610 <83501683+lixin-deng@users.noreply.github.com>
2022-10-21 12:59:04 +1030

    delete curl

--- project.o.d
+++ project.o.d
@@ -1,28 +1,6 @@
 out/emcc-release//project.o: project.cc /root/omnetpp/include/omnetpp.h \
-  MyMsg_m.h curl/curl.h curl/curlver.h curl/system.h curl/curlbuild.h \
-  curl/curlrules.h curl/easy.h curl/multi.h curl/Availability.h \
-  curl/AvailabilityVersions.h curl/AvailabilityInternal.h
+  MyMsg_m.h
 
 /root/omnetpp/include/omnetpp.h:
 
 MyMsg_m.h:
-
-curl/curl.h:
-
-curl/curlver.h:
-
-curl/system.h:
-
-curl/curlbuild.h:
-
-curl/curlrules.h:
-
-curl/easy.h:
-
-curl/multi.h:
-
-curl/Availability.h:
-
-curl/AvailabilityVersions.h:
-
-curl/AvailabilityInternal.h:
