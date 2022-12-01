d3c483731d9217ad779827e6b061183cb28dee03
Lixin Deng <denglixin0610@gmail.com>
2022-12-01 15:36:40 +1030

    e

--- project.o.d
+++ project.o.d
@@ -1,6 +1,17 @@
 out/emcc-release//project.o: project.cc /root/omnetpp/include/omnetpp.h \
-  MyMsg_m.h
+  emscripten.h emscripten/emscripten.h emscripten/em_asm.h \
+  emscripten/em_macros.h emscripten/em_js.h MyMsg_m.h
 
 /root/omnetpp/include/omnetpp.h:
+
+emscripten.h:
+
+emscripten/emscripten.h:
+
+emscripten/em_asm.h:
+
+emscripten/em_macros.h:
+
+emscripten/em_js.h:
 
 MyMsg_m.h:
